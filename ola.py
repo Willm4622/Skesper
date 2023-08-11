56f5a71913aee72d20b015dcf7347acaf1330a13
Willm4622 <140632135+Willm4622@users.noreply.github.com>
2023-08-11 11:23:45 -0300

    Update ola.py

--- ola.py
+++ ola.py
@@ -1,2 +1,61 @@
-print('Olá feito em python!') 
-print ('Olá de novo')
+def q03():
+    print ('william santos')
+
+def q02():
+    print (30,27)
+
+def q03():
+    n1 = 5
+    n2 = 8
+    n3 = 12
+
+    media = (n1 + n2 + n3)/3
+
+    print (f'A média aritmética entre 5,8 e 12 é: {media}')
+
+
+def q04():
+    numero = (input('Digite qualquer numero:'))
+
+    print (numero)
+
+def q05():
+    numero1 = float(input('digite qualquer numero:'))
+    numero2 = float(input('digite qualquer numero:'))
+
+    print (numero1)
+    print (numero2)
+
+def q06():
+    numero = int(input('Digite um número: '))
+
+    antecessor = numero-1
+    sucessor = numero+1
+
+    print (f'O antecessor é: {sucessor}')
+    print (f'O sucessor é: {antecessor}')
+ 
+def q07():
+    nome = (input('Digite seu nome'))
+    ende = (input('Digite seu endereço:'))
+    telefone = (input('Digite seu telefone:'))
+
+    print (nome)
+    print (ende)
+    print (telefone)
+
+def q08():
+    numero1 = int(input('digite qualquer numero:'))
+    numero2 = int(input('digite outro numero:'))
+
+    result = numero1-numero2 
+
+    print (f'o resultando da subtração é: {result}')
+
+def q09():
+    numero = float(input('digite qualquer numero:'))
+    
+    result = numero/4
+    print (f'O 1/4 desse numero é: {result}')
+
+q09()
